4a8b2a99386c246b3cf8c17b9c913951d803b24e
Pellizzon <matheuspellizzon@hotmail.com>
2020-03-17 14:34:49 -0300

    primeira parte

--- desprog-aps2/fourier.c
+++ desprog-aps2/fourier.c
@@ -1,58 +1,94 @@
 #include <math.h>
-
+#include <stdio.h>
 #include "fourier.h"
 
-void nft(double complex s[MAX_SIZE], double complex t[MAX_SIZE], int n, int sign) {
-  for (int k = 0; k < n; k++) {
+void nft(double complex s[MAX_SIZE], double complex t[MAX_SIZE], int n, int sign)
+{
+  for (int k = 0; k < n; k++)
+  {
     t[k] = 0;
 
-    for (int j = 0; j < n; j++) {
+    for (int j = 0; j < n; j++)
+    {
       t[k] += s[j] * cexp(sign * 2 * PI * k * j * I / n);
     }
   }
 }
 
-void nft_forward(double complex s[MAX_SIZE], double complex t[MAX_SIZE], int n) {
+void nft_forward(double complex s[MAX_SIZE], double complex t[MAX_SIZE], int n)
+{
   nft(s, t, n, -1);
 }
 
-void nft_inverse(double complex t[MAX_SIZE], double complex s[MAX_SIZE], int n) {
+void nft_inverse(double complex t[MAX_SIZE], double complex s[MAX_SIZE], int n)
+{
   nft(t, s, n, 1);
 
-  for (int k = 0; k < n; k++) {
+  for (int k = 0; k < n; k++)
+  {
     s[k] /= n;
   }
 }
 
-void fft(double complex s[MAX_SIZE], double complex t[MAX_SIZE], int n, int sign) {
+void fft(double complex s[MAX_SIZE], double complex t[MAX_SIZE], int n, int sign)
+{
+  if (n == 1)
+  {
+    t[0] = s[0];
+    return;
+  }
+
+  double complex sp[n / 2], si[n / 2], tp[n / 2], ti[n / 2];
+  for (int i = 0; i < n; i += 2)
+  {
+    sp[i / 2] = s[i];
+    si[i / 2] = s[i + 1];
+  }
+
+  fft(sp, tp, n / 2, sign);
+  fft(si, ti, n / 2, sign);
+
+  for (int k = 0; k < n / 2; k++)
+  {
+    t[k] = tp[k] + ti[k] * cexp(sign * 2 * PI * k * I / n);
+    t[k + n / 2] = tp[k] - ti[k] * cexp(sign * 2 * PI * k * I / n);
+  }
 }
 
-void fft_forward(double complex s[MAX_SIZE], double complex t[MAX_SIZE], int n) {
+void fft_forward(double complex s[MAX_SIZE], double complex t[MAX_SIZE], int n)
+{
   fft(s, t, n, -1);
 }
 
-void fft_inverse(double complex t[MAX_SIZE], double complex s[MAX_SIZE], int n) {
+void fft_inverse(double complex t[MAX_SIZE], double complex s[MAX_SIZE], int n)
+{
   fft(t, s, n, 1);
 
-  for (int k = 0; k < n; k++) {
+  for (int k = 0; k < n; k++)
+  {
     s[k] /= n;
   }
 }
 
-void fft_forward_2d(double complex matrix[MAX_SIZE][MAX_SIZE], int width, int height) {
+void fft_forward_2d(double complex matrix[MAX_SIZE][MAX_SIZE], int width, int height)
+{
 }
 
-void fft_inverse_2d(double complex matrix[MAX_SIZE][MAX_SIZE], int width, int height) {
+void fft_inverse_2d(double complex matrix[MAX_SIZE][MAX_SIZE], int width, int height)
+{
 }
 
-void filter(double complex input[MAX_SIZE][MAX_SIZE], double complex output[MAX_SIZE][MAX_SIZE], int width, int height, int flip) {
+void filter(double complex input[MAX_SIZE][MAX_SIZE], double complex output[MAX_SIZE][MAX_SIZE], int width, int height, int flip)
+{
   int center_x = width / 2;
   int center_y = height / 2;
 
   double variance = -2 * SIGMA * SIGMA;
 
-  for (int y = 0; y < height; y++) {
-    for (int x = 0; x < width; x++) {
+  for (int y = 0; y < height; y++)
+  {
+    for (int x = 0; x < width; x++)
+    {
       int dx = center_x - (x + center_x) % width;
       int dy = center_y - (y + center_y) % height;
 
@@ -60,7 +96,8 @@ void filter(double complex input[MAX_SIZE][MAX_SIZE], double complex output[MAX_
 
       double g = exp(d / variance);
 
-      if (flip) {
+      if (flip)
+      {
         g = 1 - g;
       }
 
@@ -69,10 +106,12 @@ void filter(double complex input[MAX_SIZE][MAX_SIZE], double complex output[MAX_
   }
 }
 
-void filter_lp(double complex input[MAX_SIZE][MAX_SIZE], double complex output[MAX_SIZE][MAX_SIZE], int width, int height) {
+void filter_lp(double complex input[MAX_SIZE][MAX_SIZE], double complex output[MAX_SIZE][MAX_SIZE], int width, int height)
+{
   filter(input, output, width, height, 0);
 }
 
-void filter_hp(double complex input[MAX_SIZE][MAX_SIZE], double complex output[MAX_SIZE][MAX_SIZE], int width, int height) {
+void filter_hp(double complex input[MAX_SIZE][MAX_SIZE], double complex output[MAX_SIZE][MAX_SIZE], int width, int height)
+{
   filter(input, output, width, height, 1);
 }
